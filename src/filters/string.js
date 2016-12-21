9055f4ebd6b80ac036a274054dd1da7adcf3e9f2
u3u <485463145@qq.com>
2016-12-21 19:01:09 +0800

    add string.js

---
+++ src/filters/string.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: @u3u 
+ * @Date: 2016-12-21 10:21:53 
+ * @Last Modified by: @u3u
+ * @Last Modified time: 2016-12-21 10:23:56
+ */
+
+export default {
+  // 去除文本首尾两端的空格
+  trim(s) {
+    return s.toString().trim()
+  }
+}
