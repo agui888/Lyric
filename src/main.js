8c8930b0185e94c71fa9bf738c2163dbc735447f
u3u <485463145@qq.com>
2016-12-23 00:07:41 +0800

    add custom autocomplete component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: @u3u 
  * @Date: 2016-12-21 18:19:54 
  * @Last Modified by: @u3u
- * @Last Modified time: 2016-12-21 18:22:46
+ * @Last Modified time: 2016-12-22 18:37:14
  */
 
 // The Vue build version to load with the `import` command
@@ -16,8 +16,24 @@ import 'element-ui/lib/theme-default/index.css'
 Vue.use(ElementUI)
 
 import Thread from './utils/thread'
+import QQMusicAPI from './utils/QQMusicAPI.js'
 
 /* eslint-disable no-new */
+
+Vue.component('autocomplete-singer-name', {
+  functional: true,
+  render: function (h, ctx) {
+    var item = ctx.props.item
+    return h('li', ctx.data, [
+      h('div', { attrs: { class: 'songName' } }, [QQMusicAPI.htmldecode(item.songname)]),
+      h('span', { attrs: { class: 'singerName' } }, [QQMusicAPI.htmldecode(item.singer.map(x => x.name).join('&'))])
+    ])
+  },
+  props: {
+    item: { type: Object, required: true }
+  }
+})
+
 new Vue({
   el: '#app',
   beforeCreate() {
